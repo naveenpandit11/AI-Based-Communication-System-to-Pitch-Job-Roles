20f2ce4f28d1a5b5fd951cae3574c43f3b73fe1d
Naveen Pandit <112720328+naveenpandit11@users.noreply.github.com>
2025-11-15 22:00:40 +0530

    Clean up db.js by removing commented connection string
    Remove commented-out MongoDB connection string.

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-//const mongoURL="mongodb+srv://job_emails:naveen123@cluster0.15yqtmy.mongodb.net/jobEmails?retryWrites=true&w=majority";
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL, {
